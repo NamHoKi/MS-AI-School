3bf990a0130e5ce0e83bf3f1c8307922319e348b
Ho ki Nam <48282708+NamHoKi@users.noreply.github.com>
2023-02-02 11:27:46 +0900

    Update json_label_del.py

--- my_utils/json_label_del.py
+++ my_utils/json_label_del.py
@@ -1,19 +1,19 @@
 import json
 
 path = './0201_remove_list/test.json'
+del_list = ['fence', 'park_headstone']
 with open(path, 'r', encoding='utf-8') as input_f :
     data = json.load(input_f)
 
 annos = data['annotations']
 for i in range(len(annos) - 1, -1, -1) :
-    del_check = annos[i]['object_class'] in ['fence', 'park_headstone', 'street_lamp']
+    del_check = annos[i]['object_class'] in del_list
     if del_check :
         del annos[i]
 
 category = data['category']
-
 for i in range(len(category) -1, -1, -1) :
-    del_check = category[i]['object_class_name'] in ['fence', 'park_headstone', 'street_lamp']
+    del_check = category[i]['object_class_name'] in del_list
     if del_check :
         del category[i]
 
@@ -22,3 +22,4 @@ print(json.dumps(data, ensure_ascii=False, indent=3))
 
 with open(path, 'w', encoding='utf-8') as output_f :
     json.dump(data, output_f, indent=4)
+
